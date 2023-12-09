3e56eab904c6f51487bf0bd61e8f54c5461516ec
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-12-09 09:35:32 -0800

    Console 0.0.1

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" This module contains the entry point
+of the command interpreter.
+
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ This module contains the entry point
+    of the command interpreter.
+
+    """
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, args):
+        """ This model quite the interpreter """
+
+        return True
+
+    def do_EOF(self, args):
+        """ This method quite the interpreter using ctr+D. """
+
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
