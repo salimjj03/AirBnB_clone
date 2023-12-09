5b86dd2cb6b290063efa7c5440eaf600dc5a2226
salim aliyu musawa <salemaliyu1992@gmail.co>
2023-12-09 05:48:33 -0800

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -50,7 +50,8 @@ class BaseModel:
         keys/values of __dict__ of the instance.
         """
 
-        self.__dict__["__class__"] = self.__class__.__name__
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
-        return self.__dict__
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
